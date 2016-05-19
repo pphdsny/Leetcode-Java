463e359804d6926558afba709d891a6a2a8d72ab
王鹏 <wangpeng@wangpengdeMacBook-Pro.local>
2016-05-19 23:37:06 +0800

    easyCycler

---
+++ src/pp/arithmetic/easy/HasCycleTest.java
@@ -0,0 +1,42 @@
+package pp.arithmetic.easy;
+
+/**
+ * Created by wangpeng on 16/5/19.
+ * Given a linked list, determine if it has a cycle in it.
+ * <p>
+ * Follow up:
+ * Can you solve it without using extra space?
+ * {@link https://leetcode.com/problems/linked-list-cycle/}
+ */
+public class HasCycleTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slower = head;
+        ListNode faster = head;
+        while (faster.next != null && faster.next.next != null) {
+            slower = slower.next;
+            faster = faster.next.next;
+            if (slower == faster) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
