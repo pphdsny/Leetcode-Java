68cb6bacc1fc321fbfce7e1a30d795ed775d3e28
wangpeng <pphdsny@gmail.com>
2018-08-20 15:05:44 +0800

    feat: 138-copyRandomList

---
+++ src/pp/arithmetic/model/RandomListNode.java
@@ -0,0 +1,15 @@
+package pp.arithmetic.model;
+
+/**
+ * Created by wangpeng on 2018/8/20.
+ */
+public class RandomListNode {
+    public int label;
+    public RandomListNode next;
+    public RandomListNode random;
+
+    public RandomListNode(int x) {
+        this.label = x;
+    }
+
+}
