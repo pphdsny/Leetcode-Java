988792cb78a29531ae5355f31f6af5c9122a3083
wangpeng <pphdsny@gmail.com>
2018-11-02 15:21:33 +0800

    feat: 190-reverseBits

---
+++ src/pp/arithmetic/easy/_190_reverseBits.java
@@ -0,0 +1,38 @@
+package pp.arithmetic.easy;
+
+/**
+ * Created by wangpeng on 2018/11/1.
+ * 190. 颠倒二进制位
+ * <p>
+ * 颠倒给定的 32 位无符号整数的二进制位。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 43261596
+ * 输出: 964176192
+ * 解释: 43261596 的二进制表示形式为 00000010100101000001111010011100 ，
+ * 返回 964176192，其二进制表示形式为 00111001011110000010100101000000 。
+ * 进阶:
+ * 如果多次调用这个函数，你将如何优化你的算法？
+ *
+ * @see <a href="https://leetcode-cn.com/problems/reverse-bits/description/">reverse-bits</a>
+ */
+public class _190_reverseBits {
+    public static void main(String[] args) {
+        int i = reverseBits(1);
+        System.out.println(i);
+    }
+
+    // you need treat n as an unsigned value
+    public static int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;
+            if (i < 31) {
+                result <<= 1;
+            }
+        }
+        return result;
+    }
+}
