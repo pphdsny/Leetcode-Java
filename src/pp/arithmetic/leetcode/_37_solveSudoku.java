1886844d44a0e0d81167e93d5afa75f6d8ccbbe5
wangpeng <pphdsny@126.com>
2019-05-15 11:36:24 +0800

    feat(HARD): add _37_solveSudoku

---
+++ src/pp/arithmetic/leetcode/_37_solveSudoku.java
@@ -0,0 +1,131 @@
+package pp.arithmetic.leetcode;
+
+/**
+ * Created by wangpeng on 2019-05-15.
+ * 37. 解数独
+ *
+ * 编写一个程序，通过已填充的空格来解决数独问题。
+ *
+ * 一个数独的解法需遵循如下规则：
+ *
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ * 空白格用 '.' 表示。
+ *
+ * <image src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png"></image>
+ *
+ * 一个数独。
+ *
+ * <image src="http://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Sudoku-by-L2G-20050714_solution.svg/250px-Sudoku-by-L2G-20050714_solution.svg.png"></image>
+ *
+ * 答案被标成红色。
+ *
+ * Note:
+ *
+ * 给定的数独序列只包含数字 1-9 和字符 '.' 。
+ * 你可以假设给定的数独只有唯一解。
+ * 给定数独永远是 9x9 形式的。
+ *
+ * @see <a href="https://leetcode-cn.com/problems/sudoku-solver/">sudoku-solver</a>
+ */
+public class _37_solveSudoku {
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        _37_solveSudoku solveSudoku = new _37_solveSudoku();
+        solveSudoku.solveSudoku(board);
+        print(board);
+    }
+
+    private static void print(char[][] board){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 解题思路：
+     * 数独的解法就是通过填空，不断的试，可以考虑回溯算法解决
+     * 本题是他人解法，回溯挺绕的
+     *
+     * @param board
+     */
+    public void solveSudoku(char[][] board) {
+        /**
+         * 记录某行，某位数字是否已经被摆放
+         */
+        boolean[][] row = new boolean[9][10];
+        /**
+         * 记录某列，某位数字是否已经被摆放
+         */
+        boolean[][] col = new boolean[9][10];
+        /**
+         * 记录某 3x3 宫格内，某位数字是否已经被摆放
+         */
+        boolean[][] block = new boolean[9][10];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    row[i][num] = true;
+                    col[j][num] = true;
+                    // blockIndex = i / 3 * 3 + j / 3，取整
+                    block[i / 3 * 3 + j / 3][num] = true;
+                }
+            }
+        }
+        dfs(board, row, col, block, 0, 0);
+    }
+
+    private boolean dfs(char[][] board,
+                        boolean[][] row,
+                        boolean[][] col,
+                        boolean[][] block,
+                        int i, int j) {
+        // 找寻空位置
+        while (board[i][j] != '.') {
+            if (++j >= 9) {
+                i++;
+                j = 0;
+            }
+            if (i >= 9) {
+                return true;
+            }
+        }
+        for (int num = 1; num <= 9; num++) {
+            int blockIndex = i / 3 * 3 + j / 3;
+            if (!row[i][num] && !col[j][num] && !block[blockIndex][num]) {
+                // 递归
+                board[i][j] = (char) ('0' + num);
+                row[i][num] = true;
+                col[j][num] = true;
+                block[blockIndex][num] = true;
+                if (dfs(board, row, col, block, i, j)) {
+                    return true;
+                } else {
+                    // 回溯
+                    row[i][num] = false;
+                    col[j][num] = false;
+                    block[blockIndex][num] = false;
+                    board[i][j] = '.';
+                }
+            }
+        }
+        return false;
+    }
+}
