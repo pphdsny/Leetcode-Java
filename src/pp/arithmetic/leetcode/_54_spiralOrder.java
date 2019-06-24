35178dc48473095ec7fa561e6997c5d650528d0d
wangpeng <pphdsny@126.com>
2019-06-24 13:16:27 +0800

    feat(MEDIUM): add _54_spiralOrder

---
+++ src/pp/arithmetic/leetcode/_54_spiralOrder.java
@@ -0,0 +1,88 @@
+package pp.arithmetic.leetcode;
+
+import pp.arithmetic.Util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by wangpeng on 2019-06-24.
+ * 54. 螺旋矩阵
+ * <p>
+ * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * [
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
+ * ]
+ * 输出: [1,2,3,6,9,8,7,4,5]
+ * 示例 2:
+ * <p>
+ * 输入:
+ * [
+ * [1, 2, 3, 4],
+ * [5, 6, 7, 8],
+ * [9,10,11,12]
+ * ]
+ * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/spiral-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _54_spiralOrder {
+
+    public static void main(String[] args) {
+        _54_spiralOrder spiralOrder = new _54_spiralOrder();
+        Util.printList(spiralOrder.spiralOrder(new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16},
+                {17, 18, 19, 20},
+        }));
+        Util.printList(spiralOrder.spiralOrder(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        }));
+        Util.printList(spiralOrder.spiralOrder(new int[][]{
+                {1, 2, 3}
+        }));
+    }
+
+    /**
+     * 一眼看过去，遍历很简单，如何转换成代码上的遍历逻辑，最好就是遍历一遍就把结果得到
+     * 解题思路：
+     * 通过四层循环按条件遍历结果
+     *
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0)
+            return ans;
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
+            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
+                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
+}
