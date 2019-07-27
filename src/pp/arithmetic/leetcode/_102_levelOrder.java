0676903d8ee0a501a92956d409b58e8ce08b917c
王鹏 <wangpeng03@qutoutiao.net>
2019-07-27 10:57:21 +0800

    feat(MEDIUM): add _102_levelOrder

---
+++ src/pp/arithmetic/leetcode/_102_levelOrder.java
@@ -0,0 +1,85 @@
+package pp.arithmetic.leetcode;
+
+import pp.arithmetic.Util;
+import pp.arithmetic.model.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by wangpeng on 2019-07-27.
+ * 102. 二叉树的层次遍历
+ * <p>
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * <p>
+ *  3
+ * / \
+ * 9  20
+ *  /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _102_levelOrder {
+
+    public static void main(String[] args) {
+        _102_levelOrder levelOrder = new _102_levelOrder();
+        List<List<Integer>> lists = levelOrder.levelOrder(Util.generateTreeNode());
+        for (int i = 0; i < lists.size(); i++) {
+            Util.printList(lists.get(i));
+        }
+    }
+
+    /**
+     * 解题思路：
+     * 按层次遍历，类似于BFS，用一个队列保存遍历结果
+     * 1.将（根）节点存入队列
+     * 2.将队列中数据取空
+     * 3.将取出的treeNode的左右子树存入队列并将结果存入结果集
+     * 4.重复1-3直到队列无数据
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        //1
+        queue.add(root);
+        //4
+        while (!queue.isEmpty()) {
+            List<TreeNode> list = new ArrayList<>();
+            //2
+            while (!queue.isEmpty()) {
+                list.add(queue.poll());
+            }
+            //3
+            if (list.size() > 0) {
+                List<Integer> addList = new ArrayList<>();
+                for (int i = 0; i < list.size(); i++) {
+                    TreeNode treeNode = list.get(i);
+                    addList.add(treeNode.val);
+                    if (treeNode.left != null) queue.add(treeNode.left);
+                    if (treeNode.right != null) queue.add(treeNode.right);
+                }
+                result.add(addList);
+            }
+        }
+        return result;
+    }
+}
