af25dac177d46fa609684c1efdea1cea8d6f91c8
王鹏 <wangpeng03@qutoutiao.net>
2019-12-23 19:53:56 +0800

    feat(MEDIUM): add _130_solve

---
+++ src/pp/arithmetic/leetcode/_130_solve.java
@@ -0,0 +1,88 @@
+package pp.arithmetic.leetcode;
+
+/**
+ * Created by wangpeng on 2019-12-23.
+ * 130. 被围绕的区域
+ *
+ * 给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。
+ *
+ * 找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+ *
+ * 示例:
+ *
+ * X X X X
+ * X O O X
+ * X X O X
+ * X O X X
+ * 运行你的函数后，矩阵变为：
+ *
+ * X X X X
+ * X X X X
+ * X X X X
+ * X O X X
+ * 解释:
+ *
+ * 被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/surrounded-regions
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _130_solve {
+
+    public static void main(String[] args) {
+        _130_solve solve = new _130_solve();
+        char[][] chars = {
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'X', 'X'},
+                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'X', 'X'}
+        };
+        solve.solve(chars);
+    }
+
+    /**
+     * 解题思路：
+     * 难点：如何确定一个O是否被X全部给围住
+     * char[][] board构建的其实是一张图，可以考虑使用图的DFS遍历
+     *
+     * @param board
+     */
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 从边缘O开始搜索
+                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
+                if (isEdge && board[i][j] == 'O') {
+                    dfs(board, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == 'X' || board[i][j] == '#') {
+            // board[i][j] == '#' 说明已经搜索过了.
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i - 1, j); // 上
+        dfs(board, i + 1, j); // 下
+        dfs(board, i, j - 1); // 左
+        dfs(board, i, j + 1); // 右
+    }
+}
