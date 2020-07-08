635a3424a14180048f7600bb81ec907b77301d3e
王鹏 <wangpeng03@qutoutiao.net>
2020-07-08 16:07:04 +0800

    feat(MEDIUM): add _1115_FooBar

---
+++ src/pp/arithmetic/leetcode/_1115_FooBar.java
@@ -0,0 +1,111 @@
+package pp.arithmetic.leetcode;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by wangpeng on 2020-07-08.
+ * 1115. 交替打印FooBar
+ * <p>
+ * 我们提供一个类：
+ * <p>
+ * class FooBar {
+ * public void foo() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("foo");
+ *     }
+ * }
+ * <p>
+ * public void bar() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("bar");
+ *     }
+ * }
+ * }
+ * 两个不同的线程将会共用一个 FooBar 实例。其中一个线程将会调用 foo() 方法，另一个线程将会调用 bar() 方法。
+ * <p>
+ * 请设计修改程序，以确保 "foobar" 被输出 n 次。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: n = 1
+ * 输出: "foobar"
+ * 解释: 这里有两个线程被异步启动。其中一个调用 foo() 方法, 另一个调用 bar() 方法，"foobar" 将被输出一次。
+ * 示例 2:
+ * <p>
+ * 输入: n = 2
+ * 输出: "foobarfoobar"
+ * 解释: "foobar" 将被输出两次。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/print-foobar-alternately
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _1115_FooBar {
+
+    public static void main(String[] args) {
+
+        FooBar fooBar = new FooBar(5);
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    fooBar.foo(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("foo");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    fooBar.bar(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("bar");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+
+    static class FooBar {
+        private Semaphore fooSe = new Semaphore(0);
+        private Semaphore barSe = new Semaphore(1);
+
+        private int n;
+
+        public FooBar(int n) {
+            this.n = n;
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+                barSe.acquire();
+                printFoo.run();
+                fooSe.release();
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+                fooSe.acquire();
+                printBar.run();
+                barSe.release();
+            }
+        }
+    }
+}
